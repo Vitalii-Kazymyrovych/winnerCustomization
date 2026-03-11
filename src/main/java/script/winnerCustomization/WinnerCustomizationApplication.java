1abd6db15969e94d146194f9130f03b7ef625b5c
Vitalii Kazymyrovych <vitalii.kazymyrovych@gmail.com>
2026-03-11 11:17:03 +0200

    initial

---
+++ src/main/java/script/winnerCustomization/WinnerCustomizationApplication.java
@@ -0,0 +1,13 @@
+package script.winnerCustomization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinnerCustomizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinnerCustomizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/script/winnerCustomization/WinnerCustomizationApplicationTests.java
@@ -0,0 +1,13 @@
+package script.winnerCustomization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinnerCustomizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
